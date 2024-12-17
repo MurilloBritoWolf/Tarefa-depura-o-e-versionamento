ec1be1e8167a78135ac1bc1f6cea3313512f2026
wendelvox <wendelvoxs@gmail.com>
2024-12-16 22:37:57 -0300

    Atualizado printf

---
+++ velocidade.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void exibirMenu(){
+    printf("Escolha a unidade velocidade:\n");
+    printf("1. km/h\n");
+    printf("2. m/s\n");
+    printf("3. mph\n");
+   
+}
+
+
+int main(){   
+    exibirMenu();
+    return 0;
+    
+}
