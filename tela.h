cd10e611ad3eda0bed01daf5f8fa560a62f210ce
João Murillo <131090691+MurilloBritoWolf@users.noreply.github.com>
2024-12-20 20:07:31 -0300

    Update tela.h

--- tela.h
+++ tela.h
@@ -1,15 +1,1 @@
-#ifndef TELAS_H
-#define TELAS_H
 
-// Declarações de funções para cada "tela"
-void Comprimento();
-void Massa();
-void Volume();
-void Temperatura();
-void Velocidade();
-void Eletrica();
-void Area();
-void Tempo();
-void Bits();
-
-#endif
